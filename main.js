fbbf617ed7117a220226b6b4c9f80614a444f805
Cristian Castaneda <cristianpaul_c@yahoo.com>
2021-09-07 11:52:02 +0800

    Initial Update

--- main.js
+++ main.js
@@ -61,3 +61,63 @@
 
     // Getting characters
     console.log(fullName[5]);
+
+    // String Length
+    console.log(fullName.length);
+
+    // String Methods
+    console.log(fullName.toLocaleUpperCase());
+    console.log(fullName.toLocaleLowerCase());
+    console.log(fullName.indexOf('r'));
+
+    /** Common String Methods */
+    // lastIndexOf()
+    let samp__email = 'sample@email.com';
+    console.log(samp__email.lastIndexOf('e')); // 7
+
+    // slice()
+    console.log(samp__email.slice(2, 5)); // slice(start, stop)
+
+    // substr()
+    console.log(samp__email.substr(1, 5));
+
+    // replace()
+    console.log(samp__email.replace('s', 't')); // replace('the letter', 'to be change');
+
+
+/** Numbers */
+    // Written along the code.
+    let samp__radius = 10;
+    let samp__pi = 3.14;
+
+    console.log(samp__radius, samp__pi);
+
+    // MATH OPERATOR -> BIDMAS
+    // Bracket, Indices, Division, Multi, Add, Sub
+    let bidmas__res = 5 * (3 + 10) / 4;
+    console.log(bidmas__res);
+
+    // Increment
+    let name__liked = 'Paul';
+    let like__times = 10;
+
+    console.log(name__liked + ' ' + 'has ' + like__times + ' ' + 'likes in social media');
+
+    // Template Strings
+    const the__author = 'Paul Castaneda';
+    const the__artwork = 'Programming';
+    const likes = 450;
+
+    console.log(`A book that Mr. ${the__author} made in ${the__artwork} has reached the likes from ${likes} in the first release.`);
+
+    // Template String to browser
+    let root__html = `
+        <h1> ${the__artwork} </h1>
+        <h2> ${the__author} </h2>
+        <p> reached the ${likes} in the first release. </p>
+    `
+
+    const htm__post = document.getElementById('template__string');
+    htm__post.innerHTML = root__html;
+
+/** ARRAYS */
