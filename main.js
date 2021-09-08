3d5f18b9cb7f98251543df7780ffddfa6ba636d9
Cristian Castaneda <cristianpaul_c@yahoo.com>
2021-09-08 20:52:51 +0800

    Insert else if-else

--- main.js
+++ main.js
@@ -39,4 +39,24 @@
     } while(a < 5);
 
     console.log("***************** if statement *******************");
-    
+    const my__age = 26;
+    if (my__age > 20) {
+        console.log('This is true');
+    }
+
+    // const my__pass = prompt('Enter your password');
+    // if (my__pass.length >= 8) {
+    //     console.log('You may enter');
+    // }
+
+    console.log("************** if/else statement ****************");
+    const admin__email = prompt('Enter email: ');
+    const user__password = prompt('Enter password: ');
+
+    const def__email = 'test@email.com';
+
+    if (admin__email === def__email && user__password.length >= 8) {
+        console.log('You may login');
+    } else {
+        console.log("Sorry, no account found");
+    }
