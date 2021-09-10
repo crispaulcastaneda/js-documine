38c1002a7e0434a459a387171f0941c1f138e61f
Cristian Castaneda <cristianpaul_c@yahoo.com>
2021-09-10 19:03:12 +0800

    Insert initial instructions

---
+++ main.js
@@ -0,0 +1,6 @@
+/** Objects and literals */
+// Objects have properties & things they can do (methods)
+
+// e.g blog
+// properties : 1. title, 2. content, 3. author
+// methods : 1. publish, 2. unpublish, 3. delete.
